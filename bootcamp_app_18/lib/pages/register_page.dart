53f9fa82a2174ad6ca8db7ff72d36f8470413a83
Huseyin <71729197+huseyineraslan@users.noreply.github.com>
2024-07-15 15:30:49 +0300

    Update register_page.dart
    profil sayfası değişiklikleri yapıldı tasarımı düzenlendi

--- bootcamp_app_18/lib/pages/register_page.dart
+++ bootcamp_app_18/lib/pages/register_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: RegisterPage(),
+  ));
+}
+
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
@@ -16,50 +22,118 @@ class _RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text("Kayıt Ol"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green, Colors.white70],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Çember biçiminde fotoğraf
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('lib/assets/images/profile_pic.jpg'),
+                backgroundColor: Colors.transparent,
+                child: ClipOval(
+                  child: Image(
+                    image: AssetImage('lib/assets/images/profile_pic.jpg'),
+                    fit: BoxFit.cover,
+                    width: 120,
+                    height: 120,
+                  ),
+                ),
               ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Şifre',
-                prefixIcon: Icon(Icons.lock),
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Email ile Kayıt Ol"),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.google),
-              label: Text("Google ile Kayıt Ol"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Şifre',
+                  prefixIcon: Icon(Icons.lock),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                obscureText: true,
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: FaIcon(FontAwesomeIcons.facebook),
-              label: Text("Facebook ile Kayıt Ol"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Email ile Kayıt Ol"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white70,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  // Google ile kayıt olma işlemleri burada yapılacak
+                  print("Google ile Kayıt Ol butonuna tıklandı");
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.red, Colors.yellow, Colors.green, ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FaIcon(FontAwesomeIcons.google, color: Colors.white, size: 18),
+                      SizedBox(width: 4),
+                      Text(
+                        "Google ile Kayıt Ol",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: FaIcon(FontAwesomeIcons.facebook),
+                label: Text(
+                  "Facebook ile Kayıt Ol",
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
