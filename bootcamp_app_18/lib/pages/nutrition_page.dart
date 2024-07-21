58a91e9b544b79b371491a9f2c13222798daf44b
Huseyin <71729197+huseyineraslan@users.noreply.github.com>
2024-07-21 22:28:22 +0300

    Update nutrition_page.dart
    yapay zeka beslenme sayfası

--- bootcamp_app_18/lib/pages/nutrition_page.dart
+++ bootcamp_app_18/lib/pages/nutrition_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'ai_assistant_page.dart';
+import 'statistics_page.dart';
 
 class NutritionPage extends StatelessWidget {
   const NutritionPage({super.key});
@@ -9,10 +11,73 @@ class NutritionPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Beslenme'),
       ),
-      body: const Center(
-        child: Text(
-          'Bu sayfa henüz yapılandırılmadı.',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.red, Colors.orange],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 20),
+              GradientButton(
+                text: 'Yapay Zeka Destekli Diyet Asistanı',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/aiAssistant');
+                },
+              ),
+              const SizedBox(height: 20),
+              GradientButton(
+                text: 'Vücut Kitle Endeksi Hesaplayarak Ne Yapman Gerektiğini Öğren',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/statistics');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GradientButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const GradientButton({
+    required this.text,
+    required this.onPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [Colors.green, Colors.blue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
