ffb26f78b0d1a20be6c873bb40ce162ba1d51a29
Huseyin <71729197+huseyineraslan@users.noreply.github.com>
2024-07-28 16:44:14 +0300

    Create hard_page.dart
    zor hareketler programlar sayfası

---
+++ bootcamp_app_18/lib/pages/hard_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'exercise_item.dart'; // Import this
+
+class HardProgramPage extends StatelessWidget {
+  const HardProgramPage({super.key});
+
+  final Map<String, List<String>> hardProgram = const {
+    "monday": ["Abdominals", "Hamstrings", "Quadriceps"],
+    "tuesday": ["Triceps", "Glutes", "Lats"],
+    "wednesday": ["Chest", "Adductors", "Shoulders"],
+    "thursday": ["Abductors", "Lower Back", "Neck"],
+    "friday": ["Calves", "Biceps", "Forearms"],
+    "saturday": ["Full Body"],
+    "sunday": ["Rest"]
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Zor Program'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orange, Colors.red],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: hardProgram.length,
+          itemBuilder: (context, index) {
+            String day = hardProgram.keys.elementAt(index);
+            List<String> exercises = hardProgram[day]!;
+            return Card(
+              margin: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(day),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: exercises.map((exercise) {
+                    return ExerciseItem(exercise: exercise);
+                  }).toList(),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
